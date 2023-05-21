86f70d89e88a2de7b86dc9ac66c0d46a988d1bec
Jchips <20979096+Jchips@users.noreply.github.com>
2023-05-20 18:56:17 -0700

    did the book component

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import axios from 'axios';
+import { Carousel } from 'react-bootstrap';
+import Book from './components/Book';
 
 class BestBooks extends React.Component {
   constructor(props) {
@@ -9,17 +12,34 @@ class BestBooks extends React.Component {
   }
 
   /* TODO: Make a GET request to your API to fetch all the books from the database  */
+  componentDidMount() {
+    const getBooks = () => {
+      axios
+        .get(`${process.env.REACT_APP_SERVER}/books`)
+        .then(response => this.setState({ books: response.data }, () => console.log(this.state.books)))
+        .catch(err => console.error(err));
+    };
+    getBooks();
+  }
 
   render() {
 
     /* TODO: render all the books in a Carousel */
 
+    const {books} = this.state;
+
     return (
       <>
         <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
 
-        {this.state.books.length ? (
-          <p>Book Carousel coming soon</p>
+        {this.state.books.length > 0 ? (
+          <Carousel variant='dark'>
+            {books.map(book =>
+              <Carousel.Item key={book._id}>
+                <Book book={book} />
+              </Carousel.Item>
+            )}
+          </Carousel>
         ) : (
           <h3>No Books Found :(</h3>
         )}
